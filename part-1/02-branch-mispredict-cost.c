62d7f2a01208e8dfc496916eed38b1d04e753370
Saket Saurabh <ssaurabh@cs.wisc.edu>
2016-09-15 15:29:36 -0500

    Complete branch misprediction cost for Part 1

--- part-1/02-branch-mispredict-cost.c
+++ part-1/02-branch-mispredict-cost.c
@@ -9,6 +9,7 @@
 #include <asm/unistd.h>
 
 #define BILLION 1000000000L
+#define NUM_ITERATIONS 10 // 1 million
 
 static long
 perf_event_open(struct perf_event_attr *hw_event, pid_t pid,
@@ -25,10 +26,10 @@ int
 main(int argc, char **argv)
 {
     struct perf_event_attr pe;
-    long long count;
+    long long count_1, count_2;
     int fd;
 
-    uint64_t diff;
+    uint64_t diff_1, diff_2;
     struct timespec start, end;
 
     memset(&pe, 0, sizeof(struct perf_event_attr));
@@ -45,24 +46,61 @@ main(int argc, char **argv)
        exit(EXIT_FAILURE);
     }
 
-    int i;
+    int i; 
     
-    clock_gettime(CLOCK_REALTIME, &start);	/* mark start time */
     ioctl(fd, PERF_EVENT_IOC_RESET, 0);
     ioctl(fd, PERF_EVENT_IOC_ENABLE, 0);
     
-    for (i = 3; i <= 30000; ++i) {
-	if (i == 1000);
-	else;
-    }
-
+    int sum = 0; 
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start);	/* mark start time */
+    for (i = 0; i <= NUM_ITERATIONS; ++i) { // some branch mispredictions in the for-loop condition
+	if ((i & 1) == 0) {
+		sum += 1;
+	}
+	else {
+		sum -= 1;
+	}
+    } 
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end);	/* mark the end time */
     ioctl(fd, PERF_EVENT_IOC_DISABLE, 0);
-    clock_gettime(CLOCK_REALTIME, &end);	/* mark the end time */
-    read(fd, &count, sizeof(long long));
+    
+    read(fd, &count_1, sizeof(long long));
 	
-    diff = BILLION * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec;
-    printf("%lld branch mispredicts in %llu nanoseconds \n", count, (long long unsigned int) diff);
-
+    diff_1 = BILLION * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec;
+    printf("%lld branch mispredicts in %llu nanoseconds \n", count_1, (long long unsigned int) diff_1);
+    printf("sum = %d after %d iterations\n", sum, NUM_ITERATIONS);
+    
+    printf("Re-running the experiment again when there will be different mispredictions...\n");
+    
+    ioctl(fd, PERF_EVENT_IOC_RESET, 0);
+    ioctl(fd, PERF_EVENT_IOC_ENABLE, 0);
+    
+    sum = 0;
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start);	/* mark start time */
+    for (i = 0; i <= NUM_ITERATIONS; ++i) { // some branch mispredictions in the for-loop condition
+	if ((i & 4) == 0) {
+		sum += 1;
+	}
+	else {
+		sum += 2;
+	}
+    } 
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end);	/* mark the end time */
+    ioctl(fd, PERF_EVENT_IOC_DISABLE, 0);
+    
+    read(fd, &count_2, sizeof(long long));
+	
+    diff_2 = BILLION * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec;
+    printf("%lld branch mispredicts in %llu nanoseconds \n", count_2, (long long unsigned int) diff_2);
+    printf("sum = %d after %d iterations\n", sum, NUM_ITERATIONS);
+    
+    double branch_mispredict_cost = 0.0;
+    if (count_1 < count_2) {
+	branch_mispredict_cost = ((double)(diff_2 - diff_1)) / ((double)(count_2 - count_1));
+    } else {
+    	branch_mispredict_cost = ((double)(diff_1 - diff_2)) / ((double)(count_1 - count_2));
+    }
+    printf("\nBranch misprediction cost = %f nanoseconds\n", branch_mispredict_cost);
     close(fd);
 }
 
