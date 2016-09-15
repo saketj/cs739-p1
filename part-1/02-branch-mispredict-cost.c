13d9796d814e5dd32f81f36d92dd6f9aa3c96388
Saket Saurabh <ssaurabh@cs.wisc.edu>
2016-09-15 12:27:33 -0500

    Initial Commit for part 1

--- part-1/02-branch-mispredict-cost.c
+++ part-1/02-branch-mispredict-cost.c
@@ -1,30 +1,68 @@
-#include <stdio.h>	/* for printf */
-#include <stdint.h>	/* for uint64 definition */
-#include <stdlib.h>	/* for exit() definition */
-#include <time.h>	/* for clock_gettime */
-#include <unistd.h> /* for sleep */
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <string.h>
+#include <time.h>
+#include <sys/ioctl.h>
+#include <linux/perf_event.h>
+#include <asm/unistd.h>
 
 #define BILLION 1000000000L
 
-int main(int argc, char **argv)
+static long
+perf_event_open(struct perf_event_attr *hw_event, pid_t pid,
+                int cpu, int group_fd, unsigned long flags)
 {
-	uint64_t diff;
-	struct timespec start, end;
-	int num_iterations = 1;
+    int ret;
 
-	int i;
-	clock_gettime(CLOCK_REALTIME, &start);	/* mark start time */
-	for (i = 3; i <= 30; ++i) {
-    if (i % 3 == 0) {
-      ; // do nothing
-    } else {
-      ; // do nothing
-    }
-  }
-	clock_gettime(CLOCK_REALTIME, &end);	/* mark the end time */
-
-	diff = BILLION * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec;
-	printf("elapsed time = %llu nanoseconds\n", (long long unsigned int) diff);
-
-	exit(0);
+    ret = syscall(__NR_perf_event_open, hw_event, pid, cpu,
+                   group_fd, flags);
+    return ret;
 }
+
+int
+main(int argc, char **argv)
+{
+    struct perf_event_attr pe;
+    long long count;
+    int fd;
+
+    uint64_t diff;
+    struct timespec start, end;
+
+    memset(&pe, 0, sizeof(struct perf_event_attr));
+    pe.type = PERF_TYPE_HARDWARE;
+    pe.size = sizeof(struct perf_event_attr);
+    pe.config = PERF_COUNT_HW_BRANCH_MISSES;
+    pe.disabled = 1;
+    pe.exclude_kernel = 1;
+    pe.exclude_hv = 1;
+
+    fd = perf_event_open(&pe, 0, -1, -1, 0);
+    if (fd == -1) {
+       fprintf(stderr, "Error opening leader %llx\n", pe.config);
+       exit(EXIT_FAILURE);
+    }
+
+    int i;
+    
+    clock_gettime(CLOCK_REALTIME, &start);	/* mark start time */
+    ioctl(fd, PERF_EVENT_IOC_RESET, 0);
+    ioctl(fd, PERF_EVENT_IOC_ENABLE, 0);
+    
+    for (i = 3; i <= 30000; ++i) {
+	if (i == 1000);
+	else;
+    }
+
+    ioctl(fd, PERF_EVENT_IOC_DISABLE, 0);
+    clock_gettime(CLOCK_REALTIME, &end);	/* mark the end time */
+    read(fd, &count, sizeof(long long));
+	
+    diff = BILLION * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec;
+    printf("%lld branch mispredicts in %llu nanoseconds \n", count, (long long unsigned int) diff);
+
+    close(fd);
+}
+
