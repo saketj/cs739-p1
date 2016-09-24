a45d660a943d674cb07f4d8fc7ec827fd4057119
Saket Saurabh <ssaurabh@cs.wisc.edu>
2016-09-24 16:10:27 -0500

    Complete latency experiments for gRPC

---
+++ part-3/grpc-measurements/latency/greeter_client.cc
@@ -0,0 +1,134 @@
+/*
+/bin/bash: 00: command not found
+ * Copyright 2015, Google Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following disclaimer
+ * in the documentation and/or other materials provided with the
+ * distribution.
+ *     * Neither the name of Google Inc. nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+#include <iostream>
+#include <memory>
+#include <string>
+#include <ctime>
+#include <cstdint>
+#include <cstdlib>
+#include <vector>
+#include <algorithm>
+
+#include <grpc++/grpc++.h>
+
+#include "helloworld.grpc.pb.h"
+
+using grpc::Channel;
+using grpc::ClientContext;
+using grpc::Status;
+using helloworld::HelloRequest;
+using helloworld::HelloReply;
+using helloworld::Greeter;
+
+
+#define SERVER_IP "128.105.37.193"
+#define BILLION 1000000000L
+#define NUM_ITERATIONS 100
+
+class GreeterClient {
+ public:
+  GreeterClient(std::shared_ptr<Channel> channel)
+      : stub_(Greeter::NewStub(channel)) {}
+
+  // Assambles the client's payload, sends it and presents the response back
+  // from the server.
+  char SayHello(int size) {
+    // Data we are sending to the server.
+    HelloRequest request;
+    int *arr = new int[size];
+    for (int i = 0; i < size; ++i) {
+    	request.add_data(arr[i]);
+    }
+    delete arr;
+
+    // Container for the data we expect from the server.
+    HelloReply reply;
+
+    // Context for the client. It could be used to convey extra information to
+    // the server and/or tweak certain RPC behaviors.
+    ClientContext context;
+
+    // The actual RPC.
+    Status status = stub_->SayHello(&context, request, &reply);
+
+    // Act upon its status.
+    if (status.ok()) {
+      return reply.message();
+    } else {
+      std::cout << status.error_code() << ": " << status.error_message()
+                << std::endl;
+      return '0';
+    }
+  }
+
+ private:
+  std::unique_ptr<Greeter::Stub> stub_;
+};
+
+double findMedian(std::vector<uint64_t> &vec) {
+  std::sort(vec.begin(), vec.end());
+  int size = vec.size();
+  if (size % 2 == 0) {
+    double val = (double)(vec[size/2] + vec[size/2 + 1]) / (double)2.0f;
+    return val;
+  }
+  else {
+    return vec[size/2];
+  }
+}
+
+int main(int argc, char** argv) {
+  // Instantiate the client. It requires a channel, out of which the actual RPCs
+  // are created. This channel models a connection to an endpoint (in this case,
+  // localhost at port 50051). We indicate that the channel isn't authenticated
+  // (use of InsecureChannelCredentials()).
+  GreeterClient greeter(grpc::CreateChannel(
+      "localhost:50051", grpc::InsecureChannelCredentials()));
+
+  uint64_t total_time = 0;
+  struct timespec start, end;
+  
+  for(int size = 1; size <= 8192; size = size << 1) {
+    std::vector<uint64_t> results(NUM_ITERATIONS, 0);
+    for (int itr = 0; itr < NUM_ITERATIONS; ++itr) {
+      clock_gettime(CLOCK_REALTIME, &start);	/* mark start time */
+      char reply = greeter.SayHello(size);
+      clock_gettime(CLOCK_REALTIME, &end);	/* mark end time */
+      results[itr] = BILLION * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec;
+    }
+    std::cout << "Bytes = " << (size * 4) << " RTT = " << findMedian(results) << " ns" << std::endl;
+  }
+  
+  return 0;
+}
